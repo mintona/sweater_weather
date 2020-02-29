2f0d14aa779aaaff4e1fd2d8a5123da02be79ea2
Alison Vermeil <alison.minton@gmail.com>
2020-02-29 15:26:11 -0700

    Start show action, wip

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ForecastController < ApplicationController
 
   def show
-
+    coordinates = GoogleGeocodeService.new(params["location"]).location_coordinates
+    weather_data = DarkSkyService.new(coordinates)
   end
 end
