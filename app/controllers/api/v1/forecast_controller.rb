6fae991e5c243c4c7cdfe3086beac9bd63b7b12f
Alison Vermeil <alison.minton@gmail.com>
2020-03-01 16:30:01 -0700

    Add to show action, wip

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::ForecastController < ApplicationController
 
   def show
-    coordinates = GoogleGeocodeService.new(params["location"]).location_coordinates
-    weather_data = DarkSkyService.new(coordinates)
+    # coordinates = GoogleGeocodeService.new.location_data(params["location"])
+    # weather_data = DarkSkyService.new.weather_data_by_location(coordinates)
+    forecast = Forecast.new
+    forecast.get_forecast(params["location"])
+    render json: ForecastSerializer.new(forecast)
   end
 end
