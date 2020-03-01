942b3290007719dac14df0586e6f25f1fea335f4
Alison Vermeil <alison.minton@gmail.com>
2020-03-01 10:53:55 -0700

    Add location poro

---
+++ app/poros/location.rb
@@ -0,0 +1,23 @@
+class Location
+  attr_reader :city,
+              :state,
+              :country,
+              :latitude,
+              :longitude
+
+  def initialize(location_data)
+    @city = location_data[:address_components].first[:long_name]
+    @state = location_data[:address_components][1][:short_name]
+    @country = location_data[:address_components][2][:long_name]
+    @latitude = location_data[:geometry][:location][:lat]
+    @longitude = location_data[:geometry][:location][:lng]
+  end
+
+  def city_state
+    "#{city}, #{state}"
+  end
+
+  def coordinates
+    "#{latitude},#{longitude}"
+  end
+end
