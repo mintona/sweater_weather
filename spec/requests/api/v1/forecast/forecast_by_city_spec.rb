8c9fa8a8ba7af0364159184dc2b5198ac1a701ba
Alison Vermeil <alison.minton@gmail.com>
2020-03-01 15:56:49 -0700

    Continue to build out test for forecast request

--- spec/requests/api/v1/forecast/forecast_by_city_spec.rb
+++ spec/requests/api/v1/forecast/forecast_by_city_spec.rb
@@ -13,6 +13,35 @@ describe 'Weather by City API' do
       expect(response).to be_successful
 
       forecast = JSON.parse(response.body)['data']
+
+      expect(forecast).to have_key('type')
+      expect(forecast).to have_key('attributes')
+
+      expect(forecast['attributes']).to have_key('location')
+      expect(forecast['attributes']).to have_key('country')
+
+      expect(forecast['attributes']).to have_key('current_conditions')
+
+      expect(forecast['attributes']['current_conditions']).to have_key('time')
+      expect(forecast['attributes']['current_conditions']).to have_key('summary')
+      expect(forecast['attributes']['current_conditions']).to have_key('icon')
+      # expect(forecast['attributes']['current_conditions']).to have_key('precipProbability')
+      expect(forecast['attributes']['current_conditions']).to have_key('temperature')
+      expect(forecast['attributes']['current_conditions']).to have_key('apparentTemperature')
+      expect(forecast['attributes']['current_conditions']).to have_key('humidity')
+      expect(forecast['attributes']['current_conditions']).to have_key('uvIndex')
+      expect(forecast['attributes']['current_conditions']).to have_key('visibility')
+      expect(forecast['attributes']['current_conditions']).to have_key('current_temp')
+
+      expect(forecast['attributes']).to have_key('weather_today')
+      expect(forecast['attributes']['weather_today']).to have_key('summary') #todays summary
+      expect(forecast['attributes']['weather_today']).to have_key('summary') #tonights summary
+      expect(forecast['attributes']['weather_today']).to have_key('high_temp')
+      expect(forecast['attributes']['weather_today']).to have_key('low_temp')
+
+
+      # expect(forecast['attributes']).to have_key('five_day_forecast')
+
 require "pry"; binding.pry
       #available data:
       #current weather conditions
