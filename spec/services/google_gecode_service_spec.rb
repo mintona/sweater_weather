48fe209d22028e489165b70280249f2ca2962851
Alison Vermeil <alison.minton@gmail.com>
2020-03-01 10:06:57 -0700

    Refactor google geocode service

--- spec/services/google_gecode_service_spec.rb
+++ spec/services/google_gecode_service_spec.rb
@@ -2,23 +2,39 @@ require 'rails_helper'
 
 RSpec.describe GoogleGeocodeService do
   describe 'instance methods' do
-    describe '#location_coordinates' do
-      it "returns the coordinates of a given city and state", :vcr do
+    describe '#location_data' do
+      it "returns the location data of a given city and state", :vcr => { :re_record_interval => 7.days } do
         location = "denver,co"
-        service = GoogleGeocodeService.new(location)
-        coordinates = service.location_coordinates
+        service = GoogleGeocodeService.new
+        location_info = service.location_data(location)
 
-        expect(coordinates).to be_a String
-        expect(coordinates).to eq("39.7392358,-104.990251")
+        expect(location_info).to be_a Hash
+        expect(location_info[:results].first[:address_components].first).to have_key(:long_name)
+        expect(location_info[:results].first[:address_components].first[:long_name]).to eq("Denver")
+
+        expect(location_info[:results].first[:address_components][2]).to have_key(:short_name)
+        expect(location_info[:results].first[:address_components][2][:short_name]).to eq("CO")
+
+        expect(location_info[:results].first[:address_components][3]).to have_key(:long_name)
+        expect(location_info[:results].first[:address_components][3][:long_name]).to eq("United States")
+
+        expect(location_info[:results].first[:geometry]).to have_key(:location)
+        expect(location_info[:results].first[:geometry][:location]).to have_key(:lat)
+        expect(location_info[:results].first[:geometry][:location]).to have_key(:lng)
+        expect(location_info[:results].first[:geometry][:location][:lat]).to eq(39.7392358)
+        expect(location_info[:results].first[:geometry][:location][:lng]).to eq(-104.990251)
+
+        expect(location_info[:status]).to eq("OK")
       end
 
-      it "handles bad input", :vcr do
+      it "returns empty array if location not found", :vcr => { :re_record_interval => 7.days } do
         location = "notaplace"
-        service = GoogleGeocodeService.new(location)
-        coordinates = service.location_coordinates
+        service = GoogleGeocodeService.new
+        location_info = service.location_data(location)
 
-        expect(coordinates).to be_a String
-        expect(coordinates).to eq("invalid location")
+        expect(location_info).to be_a Hash
+        expect(location_info[:results]).to eq([])
+        expect(location_info[:status]).to eq("ZERO_RESULTS")
       end
     end
   end
