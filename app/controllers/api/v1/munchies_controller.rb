a7803e5380a1349ff0f9549af76315b4f1250596
Alison Vermeil <alison.minton@gmail.com>
2020-03-02 09:23:59 -0700

    Add munchies controller with show action

---
+++ app/controllers/api/v1/munchies_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::MunchiesController < ApplicationController
+
+  def show
+    start = params['start']
+    destination = params['end']
+    food = params['food']
+
+require "pry"; binding.pry
+  end
+
+end
