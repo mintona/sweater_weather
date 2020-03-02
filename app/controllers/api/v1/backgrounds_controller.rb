95f64e6d6a63ae52d07f6d90db8478dc75c08944
Alison Vermeil <alison.minton@gmail.com>
2020-03-01 21:41:41 -0700

    Refactor controller with new background search and method

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BackgroundsController < ApplicationController
   def show
-    background = Background.new(params[:location])
+    background = BackgroundSearch.new.get_background(params[:location])
     render json: BackgroundSerializer.new(background)
   end
 end
