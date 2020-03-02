d869e96f9594d6ce9cc3e3d585e2690ecbf35bfd
Alison Vermeil <alison.minton@gmail.com>
2020-03-02 11:47:04 -0700

    Clean up munchies controller, test still passing and working in local host

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,14 +1,6 @@
 class Api::V1::MunchiesController < ApplicationController
-
   def show
-    start = params['start']
-    destination = params['end']
-    food = params['food']
-
-    travel_time = GoogleGeocodeService.new.travel_data(start, destination)
-
-    munchie = MunchieFacade.new(start, destination, food)
+    munchie = MunchieFacade.new(params['start'], params['end'], params['food'])
     render json: MunchieSerializer.new(munchie)
   end
-
 end
