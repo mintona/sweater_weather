f110dc71b795cc1f87a6b62b3893bc6508eecb9d
Alison Vermeil <alison.minton@gmail.com>
2020-03-01 18:05:46 -0700

    Add tests for weather instance methods

--- spec/poros/weather_spec.rb
+++ spec/poros/weather_spec.rb
@@ -10,56 +10,93 @@ RSpec.describe Weather do
 
   it 'exists' do
     expect(@weather).to be_a Weather
-    # expect(weather.six_day_forecast.count).to eq(5)
   end
 
   describe "instance methods" do
-    describe "#six_day_forecast" do
-      it "returns specific weather information for following 5 days" do
-        six_day_forecast = @weather.six_day_forecast
+    describe "#current_conditions" do
+      it "returns the current conditions" do
+        current_conditions = @weather.current_conditions
 
-        expect(six_day_forecast).to be_a Array
-        expect(six_day_forecast.count).to eq(6)
-        expect(six_day_forecast.first).to have_key(:time)
-        expect(six_day_forecast.first).to have_key(:summary)
-        expect(six_day_forecast.first).to have_key(:icon)
-        expect(six_day_forecast.first).to have_key(:precipProbability)
-        expect(six_day_forecast.first).to have_key(:precipType)
-        expect(six_day_forecast.first).to have_key(:temperatureLow)
-        expect(six_day_forecast.first).to have_key(:temperatureHigh)
+        keys = [
+                :time,
+                :summary,
+                :icon,
+                :precipProbability,
+                :temperature,
+                :apparentTemperature,
+                :humidity,
+                :uvIndex,
+                :visibility
+              ]
+
+        expect(current_conditions).to be_a Hash
+        expect(current_conditions.keys).to match_array(keys)
+        expect(current_conditions[:time]).to eq(1583006534)
+        expect(current_conditions[:summary]).to eq("Overcast")
+        expect(current_conditions[:icon]).to eq("cloudy")
+        expect(current_conditions[:precipProbability]).to eq(0)
+        expect(current_conditions[:temperature]).to eq(52.39)
+        expect(current_conditions[:apparentTemperature]).to eq(52.39)
+        expect(current_conditions[:humidity]).to eq(0.19)
+        expect(current_conditions[:uvIndex]).to eq(3)
+        expect(current_conditions[:visibility]).to eq(10)
       end
     end
 
-    describe "#current_temperature" do
-      it "returns the current temperature" do
-        current_temp = @weather.current_temp
+    describe "#weather_today" do
+      it "returns the forcast for the day" do
+        weather_today = @weather.weather_today
 
-        expect(current_temp).to eq(52.39)
+        keys = [
+                :summary,
+                :temperatureHigh,
+                :temperatureLow,
+                :icon
+              ]
+        expect(weather_today).to be_a Hash
+        expect(weather_today.keys).to match_array(keys)
+        expect(weather_today[:summary]).to eq("Overcast throughout the day.")
+        expect(weather_today[:icon]).to eq("cloudy")
+        expect(weather_today[:temperatureHigh]).to eq(59.08)
+        expect(weather_today[:temperatureLow]).to eq(31.68)
       end
     end
 
-    describe "#high_temp_today" do
-      it "returns the forcasted high temp for today" do
-        high_temp = @weather.high_temp_today
+    describe "#next_eight_hours" do
+      it "returns the forecast for the next 8 hours" do
+        eight_hours = @weather.next_eight_hours
 
-        expect(high_temp).to eq(59.08)
+        expect(eight_hours).to be_a Array
+
+        expect(eight_hours.first.keys).to match_array([:time, :icon, :temperature])
+        expect(eight_hours.first[:time]).to eq(1583006400)
+        expect(eight_hours.first[:icon]).to eq("cloudy")
+        expect(eight_hours.first[:temperature]).to eq(52.28)
       end
     end
 
-    # describe "#low_temp_today" do
-    #   it "returns the forcasted high temp for today" do
-    #     low_temp = @weather.low_temp_today
-    #
-    #     expect(low_temp).to eq(31.68)
-    #   end
-    # end
-    #
-    # describe "#current_feels_like" do
-    #   it "returns current appparent temperature" do
-    #     feels_like = @weather.current_feels_like
-    #
-    #     expect(feels_like)
-    #   end
-    # end
+    describe "#next_five_days" do
+      it "returns the forcast for the next 5 days" do
+        five_days = @weather.next_five_days
+        keys = [
+                :time,
+                :icon,
+                :precipType,
+                :precipProbability,
+                :temperatureHigh,
+                :temperatureLow
+              ]
+        expect(five_days).to be_a Array
+
+        expect(five_days.count).to eq(5)
+        expect(five_days.first.keys).to match_array(keys)
+        expect(five_days.first[:time]).to eq(1583046000)
+        expect(five_days.first[:icon]).to eq("snow")
+        expect(five_days.first[:precipProbability]).to eq(0.5)
+        expect(five_days.first[:precipType]).to eq("snow")
+        expect(five_days.first[:temperatureHigh]).to eq(52.01)
+        expect(five_days.first[:temperatureLow]).to eq(23.76)
+      end
+    end
   end
 end
