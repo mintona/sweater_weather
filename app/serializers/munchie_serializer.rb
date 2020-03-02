86390184274c19d38ff17cc82f581560c0d5af64
Alison Vermeil <alison.minton@gmail.com>
2020-03-02 11:32:28 -0700

    Remove id from serializer

--- app/serializers/munchie_serializer.rb
+++ app/serializers/munchie_serializer.rb
@@ -1,7 +1,6 @@
 class MunchieSerializer
   include FastJsonapi::ObjectSerializer
-  attributes  :id,
-              :end_location,
+  attributes  :end_location,
               :travel_time,
               :forecast,
               :restaurant
