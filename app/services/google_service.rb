a9bbf8d5a0c3b5f6318a81ea291bb139c916cb87
Alison Vermeil <alison.minton@gmail.com>
2020-03-03 15:38:44 -0700

    Refactor google service to make it more dynamic

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -1,17 +1,17 @@
 class GoogleService
   def location_data(location)
-    get_json(location)
+    get_json('geocode/json', { address: location })
   end
 
   private
 
   def conn
-    Faraday.new(url: 'https://maps.googleapis.com/maps/api/geocode')
+    Faraday.new(url: 'https://maps.googleapis.com/maps/api')
   end
 
-  def get_json(location)
-    response = conn.get('json') do |req|
-      req.params['address'] = location
+  def get_json(url, params)
+    response = conn.get(url) do |req|
+      req.params = params
       req.params['key'] = ENV['GOOGLE_GEOCODE_KEY']
     end
     JSON.parse(response.body, symbolize_names: true)[:results].first
