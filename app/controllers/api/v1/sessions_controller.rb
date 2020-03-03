c6aa9d053fe196dc39031e567c8426c77af89363
Alison Vermeil <alison.minton@gmail.com>
2020-03-03 10:37:09 -0700

    Refactor create action

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,12 +2,10 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if !user.nil? && user.authenticate(params[:password])
-      response.status = 200
       render json: { api_key: user.api_key}
     else
-      response.status = 401
       response.body = "Credentials are bad."
-      render json: response.body
+      render json: response.body, status: :unauthorized
     end
   end
 end
