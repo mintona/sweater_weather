b00ec2f1606ad05eb0966e3ca930f6a5b0820382
Alison Vermeil <alison.minton@gmail.com>
2020-03-03 16:35:31 -0700

    Turn of VCR, fix typo

--- spec/requests/api/v1/road_trip/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip/road_trip_request_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 describe "Road trip API" do
   describe 'endpoints' do
+    VCR.turn_off! :ignore_cassettes => true
+    WebMock.allow_net_connect!
+
     it 'retrieves the travel time and arrival forecast for a given trip' do
       user = User.create!(email: "myemail@gmail.com",
                           password: "password",
@@ -25,8 +28,8 @@ describe "Road trip API" do
       expect(trip_info['attributes']).to have_key('destination')
       expect(trip_info['attributes']).to have_key('travel_time')
       expect(trip_info['attributes']).to have_key('arrival_forecast')
-      expect(trip_info['attributes']['forecast']).to have_key('temperature')
-      expect(trip_info['attributes']['forecast']).to have_key('summary')
+      expect(trip_info['attributes']['arrival_forecast']).to have_key('temperature')
+      expect(trip_info['attributes']['arrival_forecast']).to have_key('summary')
     end
   end
 end
