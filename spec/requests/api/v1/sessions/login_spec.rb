4a804b8cec8ff0ba30033c0f3573bd03aa1a7db6
Alison Vermeil <alison.minton@gmail.com>
2020-03-02 21:04:15 -0700

    Add happy and sad path testing for sessions endpoint

--- spec/requests/api/v1/sessions/login_spec.rb
+++ spec/requests/api/v1/sessions/login_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Sessions API' do
   describe 'endpoints' do
     describe 'successful login' do
       it 'returns the users api key' do
-        user = User.create!(email: "myemail@email",
+        user = User.create!(email: "myemail@email.com",
                             password: "password",
                             password_confirmation: "password")
 
@@ -17,11 +17,75 @@ RSpec.describe 'Sessions API' do
 
         expect(response.status).to eq(200)
 
-        expected_body = {
-                          "api_key": "jgn983hy48thw9begh98h4539h4",
-                        }
-                        
-        expect(response.body).to eq(expected_body)
+        parsed_response = JSON.parse(response.body)
+
+        expect(parsed_response).to have_key('api_key')
+      end
+    end
+
+    describe 'unsuccessful login' do
+      describe 'wrong password' do
+        it 'returns a 401 status code and error message' do
+          user = User.create!(email: "itsme@email.com",
+                              password: "password",
+                              password_confirmation: "password")
+
+          params = {
+                    email: "itsme@email.com",
+                    password: "notmypassword"
+                  }
+
+          post '/api/v1/sessions', params: params
+
+          expect(response.status).to eq(401)
+          expect(response.body).to eq("Credentials are bad.")
+        end
+      end
+
+      describe 'invalid email' do
+        it 'returns a 401 status code and error message' do
+          user = User.create!(email: "itsme@email.com",
+                              password: "password",
+                              password_confirmation: "password")
+
+          params = {
+                    email: "noonesemail",
+                    password: "password"
+                  }
+
+          post '/api/v1/sessions', params: params
+
+          expect(response.status).to eq(401)
+          expect(response.body).to eq("Credentials are bad.")
+        end
+      end
+
+      describe 'missing field' do
+        it 'returns 401 status code and error message' do
+          user = User.create!(email: "itsme@email.com",
+                              password: "password",
+                              password_confirmation: "password")
+
+          params = {
+                    email: "itsme@email.com",
+                    password: nil
+                  }
+
+          post '/api/v1/sessions', params: params
+
+          expect(response.status).to eq(401)
+          expect(response.body).to eq("Credentials are bad.")
+
+          params = {
+                    email: nil,
+                    password: "password"
+                  }
+
+          post '/api/v1/sessions', params: params
+
+          expect(response.status).to eq(401)
+          expect(response.body).to eq("Credentials are bad.")
+        end
       end
     end
   end
