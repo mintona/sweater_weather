ce029dbf30b33c28c0f22476f77c11dba6067504
Alison Vermeil <alison.minton@gmail.com>
2020-03-02 19:37:50 -0700

    Add sad path tests

--- spec/requests/api/v1/users/create_user_spec.rb
+++ spec/requests/api/v1/users/create_user_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.describe 'Users API' do
   describe 'endpoints' do
     describe 'successful' do
-      # A successful request creates a user and generates a unique api
-      # key associated with that user.
       it 'creates a new user and generates and api key' do
           params = {
                       email: "whatever@example.com",
@@ -15,18 +13,71 @@ RSpec.describe 'Users API' do
         post '/api/v1/users', params: params
 
         expect(response.status).to eq(201)
-        expected_body = { "api_key": "some key"}
-        actual_body = JSON.parse(response.body)
 
-        expect(actual_body).to eq(expected_body)
+        body = JSON.parse(response.body)["data"]["attributes"]
+
+        expect(body).to have_key("api_key")
       end
     end
 
     describe 'unsuccessful' do
-# An unsuccessful request returns a 400 level status code and body
-# with a description of why the request wasn’t successful.
-# Potential reasons a request would fail: passwords don’t match,
-# email has already been taken, missing a field, etc.
+      describe 'mismatched passwords' do
+        it 'returns a 401 with description of why request was not successful' do
+          params = {
+                      email: "whatever@example.com",
+                      password: "password",
+                      password_confirmation: "doesnnotmatch"
+                    }
+
+          post '/api/v1/users', params: params
+
+          expect(response.status).to eq(401)
+          expect(response.body).to eq("Password confirmation doesn't match Password")
+        end
+      end
+
+      describe 'already taken username' do
+        it "returns a 401 with description of why request was not successful" do
+          user = User.create!(email: "newemail@email.com", password: "password", password_confirmation: "password")
+
+          params = {
+                      email: "newemail@email.com",
+                      password: "password",
+                      password_confirmation: "password"
+                    }
+
+          post '/api/v1/users', params: params
+
+          expect(response.status).to eq(401)
+          expect(response.body).to eq("Email has already been taken")
+        end
+      end
+
+      describe 'missing field' do
+        it "returns a 401 with description of why request was not successful" do
+          params = {
+                      email: "newemail@email.com",
+                      password: "password",
+                      password_confirmation: nil
+                    }
+
+          post '/api/v1/users', params: params
+
+          expect(response.status).to eq(401)
+          expect(response.body).to eq("Password confirmation can't be blank")
+
+          params = {
+                      email: nil,
+                      password: "password",
+                      password_confirmation: "password"
+                    }
+
+          post '/api/v1/users', params: params
+
+          expect(response.status).to eq(401)
+          expect(response.body).to eq("Email can't be blank")
+        end
+      end
     end
   end
 end
