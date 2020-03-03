9195e8f835028daa13f06071e9b5c0f83a67b885
Alison Vermeil <alison.minton@gmail.com>
2020-03-02 20:10:09 -0700

    Refactor to use has_secure_token rails method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  def generate_api_key
-    SecureRandom.base58(27)
-  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,19 +8,4 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :password_confirmation }
     it { should validate_uniqueness_of :api_key }
   end
-
-  describe 'model methods' do
-    describe 'set_api_key' do
-      it 'sets the api_key' do
-        user = User.new(email: "email@email.com", password: "password", password_confirmation: "password")
-
-        expect(user.api_key).to be_nil
-        user.set_api_key
-
-        expect(user.api_key).to_not be_nil
-        expect(user.api_key).to be_a String
-        expect(user.api_key.length).to eq(27)
-      end
-    end
-  end
 end
