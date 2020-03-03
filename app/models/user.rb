b48b4db9ca954d2c8b0ca7acd1276a5509621cd0
Alison Vermeil <alison.minton@gmail.com>
2020-03-02 19:24:34 -0700

    Add password confirmation validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
+  validates_presence_of :password_confirmation, require: true
   validates :api_key, uniqueness: true, presence: false
 
   has_secure_password
