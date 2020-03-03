897fff8354b413ee0e93dbafda6b7779ad83fae7
Alison Vermeil <alison.minton@gmail.com>
2020-03-02 17:15:27 -0700

    Add validations and strong password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :password, require: true
+  validates :api_key, uniqueness: true, presence: true
+
+  has_secure_password
 end
