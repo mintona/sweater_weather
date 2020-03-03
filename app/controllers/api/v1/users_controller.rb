a6641799cde7739c3090d85120d5fea8f184f7eb
Alison Vermeil <alison.minton@gmail.com>
2020-03-02 19:39:03 -0700

    Add to users create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    user = User.create!(user_params)
+    user = User.new(user_params)
+    if user.save
+      response.status = 201
+      render json: UserSerializer.new(user)
+    else
+      response.status = 401
+      render json: user.errors.full_messages.to_sentence
+    end
   end
 
   private
