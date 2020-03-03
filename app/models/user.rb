04d1844d8923680fdd4c2f8dc07e3876be6be582
Alison Vermeil <alison.minton@gmail.com>
2020-03-02 17:52:31 -0700

    Add before action for set_api_key with method and un-require presence of api_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+  before_create :set_api_key
+
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
-  validates :api_key, uniqueness: true, presence: true
+  validates :api_key, uniqueness: true, presence: false
 
   has_secure_password
+
+  def set_api_key
+    self.api_key = generate_api_key
+  end
 end
