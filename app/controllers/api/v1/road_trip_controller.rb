5b37736c29b516435cb4efb68ed20eee9eace0e9
Alison Vermeil <alison.minton@gmail.com>
2020-03-03 18:17:30 -0700

    Clean up controller and use response serializer

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -2,10 +2,7 @@ class Api::V1::RoadTripController < ApplicationController
   def create
     if params[:api_key].nil? || !User.exists?(api_key: params[:api_key])
       response.status = :unauthorized
-      # error = NotSuccessful.new(response)
-      # require "pry"; binding.pry
-      render json: NotSuccessfulSerializer.new(NotSuccessful.new(response))
-      # render json: response.message, status: :unauthorized
+      render json: ResponseSerializer.new(Response.new(response))
     else
       road_trip = RoadTripFacade.new(road_trip_params)
       render json: RoadTripSerializer.new(road_trip), status: :created
