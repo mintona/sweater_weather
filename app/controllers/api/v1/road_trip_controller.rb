37d451b487422e0017747e1f383a1326e12c0bcd
Alison Vermeil <alison.minton@gmail.com>
2020-03-03 17:58:16 -0700

    Dream drive refactor to use not successful poro

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    road_trip = RoadTripFacade.new(road_trip_params)
-    render json: RoadTripSerializer.new(road_trip), status: :created
+    if params[:api_key].nil? || !User.exists?(api_key: params[:api_key])
+      response.status = :unauthorized
+      # error = NotSuccessful.new(response)
+      # require "pry"; binding.pry
+      render json: NotSuccessfulSerializer.new(NotSuccessful.new(response))
+      # render json: response.message, status: :unauthorized
+    else
+      road_trip = RoadTripFacade.new(road_trip_params)
+      render json: RoadTripSerializer.new(road_trip), status: :created
+    end
   end
 
   private
