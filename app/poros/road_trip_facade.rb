f279a3aed590eff2fc75d81ef2f56970dd179933
Alison Vermeil <alison.minton@gmail.com>
2020-03-03 17:01:15 -0700

    Add remaining methods to facade

--- app/poros/road_trip_facade.rb
+++ app/poros/road_trip_facade.rb
@@ -11,6 +11,26 @@ class RoadTripFacade
   end
 
   def travel_time
-require "pry"; binding.pry
+    road_trip.duration
+  end
+
+  def arrival_time
+    current_time = Time.now.to_i
+    current_time + road_trip.duration_in_seconds
+  end
+
+  def arrival_forecast
+    weather_info = dark_sky_service.get_future_forecast(road_trip.destination_latitude, road_trip.destination_longitude, arrival_time)
+    CurrentWeather.new(weather_info)
+  end
+
+  private
+
+  def road_trip
+    @road_trip ||= RoadTrip.new(GoogleService.new.trip_data(@origin, @destination))
+  end
+
+  def dark_sky_service
+    DarkSkyService.new
   end
 end
