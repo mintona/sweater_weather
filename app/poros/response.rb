4f9a8f696743a597f690b8450def1e55cde1e388
Alison Vermeil <alison.minton@gmail.com>
2020-03-03 18:51:25 -0700

    Add body to response attributes

--- app/poros/response.rb
+++ app/poros/response.rb
@@ -1,11 +1,13 @@
 class Response
   attr_reader :id,
               :status,
-              :message
+              :message,
+              :body
 
   def initialize(response)
     @id = nil
     @status = response.status
     @message = response.message
+    @body = response.body if response.body != ""
   end
 end
